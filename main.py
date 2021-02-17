91acfd109f4a4b6a8a7486de3cfb660992e8b176
siddhesh-sinha <53999351+siddhesh-sinha@users.noreply.github.com>
2021-02-17 18:35:14 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,3 @@
+import test_package
+test_pack = test_package.cpu_bound()
+test_pack.print('siddhesh')
---
+++ test_package/__init__.py
@@ -0,0 +1,5 @@
+class cpu_bound():
+    def __init__(self):
+        self._ = 0
+    def print(self,name):
+        print(f'hello ,{name}')
